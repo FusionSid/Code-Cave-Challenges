1f421b09e5f6dda7a66eb25671d630b6c834a212
FusionSid <siddheshadsv@icloud.com>
2023-05-17 21:02:34 +1200

    fix: remove unnececary for loop

--- base64/base64_converter.py
+++ base64/base64_converter.py
@@ -2,29 +2,32 @@ def base64_encode(text: str, hex: bool = False) -> str:
     BASE_64_ALPHABET = (
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
     )
+
     # convert entire thing to binary
-    if hex:
-        binary = "{:08b}".format(int(bytes.fromhex(text).hex(), 16))
-    else:
-        binary = "".join(format(ord(i), "08b") for i in text)
+    binary = (
+        "{:08b}".format(int(bytes.fromhex(text).hex(), 16))
+        if hex
+        else "".join(format(ord(i), "08b") for i in text)
+    )
 
     # split into 6 bit chunks
     chunks = [[j for j in binary[i : i + 6]] for i in range(0, len(binary), 6)]
-    # convert those binary to base 10
-    chunks = [int("".join(i), base=2) if len(i) == 6 else i for i in chunks]
 
-    final = ""
-    for i in chunks[:]:
-        if not isinstance(i, int):
-            # if length of chunk is too small, add paddding of "0"s
-            i = int("".join(i) + ("0" * (6 - len(i))), base=2)
-        final += BASE_64_ALPHABET[i]
+    # convert all binary numbers to base10 for array index
+    chunks = [
+        int("".join(i), base=2)
+        if len(i) == 6
+        else int("".join(i) + ("0" * (6 - len(i))), base=2)
+        for i in chunks
+    ]
 
-    # final padding to make the length a multiple of 4
-    padding = abs(len(final) - (4 * round(len(final) / 4))) * "="
-    encoded = final + padding
+    # encoded result without any padding yet
+    pre_padding = "".join(BASE_64_ALPHABET[i] for i in chunks)
 
-    return encoded
+    # add final padding to make the length a multiple of 4
+    padding = abs(len(pre_padding) - (4 * round(len(pre_padding) / 4))) * "="
+
+    return pre_padding + padding
 
 
 def test():
